320b7828ac813e31a82b514e5f83b3bcba63f0c0
Karim Aouaouda <110925809+karimaouaouda@users.noreply.github.com>
2023-12-16 21:10:22 +0100

    finish admin

--- database/migrations/2023_12_08_204119_create_modules_table.php
+++ database/migrations/2023_12_08_204119_create_modules_table.php
@@ -11,6 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
+        Schema::dropIfExists('modules');
         Schema::create('modules', function (Blueprint $table) {
             $table->string("name")->primary();
             $table->timestamps();
